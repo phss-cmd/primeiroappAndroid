d49c7847f451b489d2faebfe95272d15b1776981
phss-cmd <ps12.ps63@gmail.com>
2022-06-02 11:57:31 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image,Dimensions,ScrollView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <ScrollView>
+    <View style={estilo.container}>
+      <Text style={estilo.titulo} >Jogos do Gameplay</Text>
       <StatusBar style="auto" />
+      <Text style={estilo.titulo} >Minecraft</Text>
+      <Image style={estilo.img} source={require("./assets/Minecraft_capa.png")} />
+      <Text style={estilo.titulo} >Forza Horizon 5</Text>
+      <Image style={estilo.img} source={require("./assets/Forza_Horizon_5.jpg")} />
     </View>
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
+const estilo = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000814',
     alignItems: 'center',
-    justifyContent: 'center',
+    
   },
+  titulo:{
+    fontSize:40,
+    marginTop:20,
+    color:"#118ab2",
+  },
+  img:{
+    width:200,
+    height:300,
+    borderRadius:10,
+  }
 });
